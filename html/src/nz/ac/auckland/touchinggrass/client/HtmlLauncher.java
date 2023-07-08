9d17d15f4e0d3c3610ac1a03f87026939905945d
Matthew Jakeman <mjakeman26@outlook.co.nz>
2023-07-08 12:50:54 +1200

    Setup libgdx

---
+++ html/src/nz/ac/auckland/touchinggrass/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package nz.ac.auckland.touchinggrass.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import nz.ac.auckland.touchinggrass.TouchingGrass;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new TouchingGrass();
+        }
+}
