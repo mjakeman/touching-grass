7c976278dfb260f711a50ff954705daa81ede819
Matthew Jakeman <mjakeman26@outlook.co.nz>
2023-07-10 17:12:41 +1200

    Improve HTML embedding page

--- html/src/nz/ac/auckland/touchinggrass/client/HtmlLauncher.java
+++ html/src/nz/ac/auckland/touchinggrass/client/HtmlLauncher.java
@@ -3,6 +3,9 @@ package nz.ac.auckland.touchinggrass.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.preloader.Preloader;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.user.client.ui.Panel;
 import nz.ac.auckland.touchinggrass.TouchingGrass;
 
 public class HtmlLauncher extends GwtApplication {
@@ -10,10 +13,22 @@ public class HtmlLauncher extends GwtApplication {
         @Override
         public GwtApplicationConfiguration getConfig () {
                 // Resizable application, uses available space in browser
-//                return new GwtApplicationConfiguration(true);
+                GwtApplicationConfiguration config = new GwtApplicationConfiguration(true);
+                config.padHorizontal = 0;
+                config.padVertical = 0;
+                return config;
                 // Fixed size application:
-                return new GwtApplicationConfiguration(1280, 720);
+                //return new GwtApplicationConfiguration(480, 320);
         }
+
+        @Override
+        protected void adjustMeterPanel(Panel meterPanel, Style meterStyle) {
+                meterPanel.setStyleName("gdx-meter");
+                meterPanel.addStyleName("nostripes");
+                meterStyle.setProperty("backgroundColor", "#ffffff");
+                meterStyle.setProperty("backgroundImage", "none");
+        }
+
 
         @Override
         public ApplicationListener createApplicationListener () {
